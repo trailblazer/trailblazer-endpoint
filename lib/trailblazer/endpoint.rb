1121979e1153faf7f7ff88861ce7e7330298daa8
Rui Baltazar <rui.p.baltazar@gmail.com>
2017-11-20 14:22:03 +0800

    WIP - re-worked endpoint concept

--- lib/trailblazer/endpoint.rb
+++ lib/trailblazer/endpoint.rb
@@ -1,54 +1,82 @@
-require "dry/matcher"
-
 module Trailblazer
   class Endpoint
-    # this is totally WIP as we need to find best practices.
-    # also, i want this to be easily extendable.
-    Matcher = Dry::Matcher.new(
-      present: Dry::Matcher::Case.new( # DISCUSS: the "present" flag needs some discussion.
-        match:   ->(result) { result.success? && result["present"] },
-        resolve: ->(result) { result }),
-      success: Dry::Matcher::Case.new(
-        match:   ->(result) { result.success? },
-        resolve: ->(result) { result }),
-      created: Dry::Matcher::Case.new(
-        match:   ->(result) { result.success? && result["model.action"] == :new }, # the "model.action" doesn't mean you need Model.
-        resolve: ->(result) { result }),
-      not_found: Dry::Matcher::Case.new(
-        match:   ->(result) { result.failure? && result["result.model"] && result["result.model"].failure? },
-        resolve: ->(result) { result }),
-      # TODO: we could add unauthorized here.
-      unauthenticated: Dry::Matcher::Case.new(
-        match:   ->(result) { result.failure? && result["result.policy.default"].failure? }, # FIXME: we might need a &. here ;)
-        resolve: ->(result) { result }),
-      invalid: Dry::Matcher::Case.new(
-        match:   ->(result) { result.failure? && result["result.contract.default"] && result["result.contract.default"].failure? },
-        resolve: ->(result) { result })
-    )
+    DEFAULT_MATCHERS = {
+      created: {
+        rule: ->(result) { result.success? && result["model.action"] == :new },
+        resolve: ->(result, representer) do
+          {
+            "data": representer.new(result["model"]),
+            "status": :created
+          }
+        end
+      },
+      success: {
+        rule: ->(result) { result.success? },
+        resolve: ->(result, representer) do
+          {
+            "data": representer.new(result["results"]),
+            "status": :ok
+          }
+        end
+      },
+      unauthenticated: {
+        rule: ->(result) { result.policy_error? },
+        resolve: ->(_result, _representer) do
+          {
+            "data": {},
+            "status": :unauthorized
+          }
+        end
+      },
+      invalid: {
+        # rule: ->(result) { result.failure? && result["result.contract.default"]&.failure? },
+        rule: ->(result) { result.failure? },
+        resolve: ->(_result, _representer) do
+          {
+            "data": {},
+            "status": :unprocessable_entity
+          }
+        end
+      },
+      not_found: {
+        rule: ->(result) { result.failure? && result["result.model"]&.failure? },
+        resolve: ->(result, _representer) do
+          {
+            "data": { errors: result["result.model.errors"] },
+            "status": :unprocessable_entity
+          }
+        end
+      }
+    }
 
-    # `call`s the operation.
-    def self.call(operation_class, handlers, *args, &block)
-      result = operation_class.(*args)
-      new.(result, handlers, &block)
+    # options expects a TRB Operation result
+    # it might have a representer, else will assume the default name
+    def self.call(operation_result, representer_class = nil, overrides = {})
+      # TODO: What to do when nothing matches?
+      endpoint_opts = { result: operation_result, representer: representer_class }
+      new.(endpoint_opts, overrides)
     end
 
-    def call(result, handlers=nil, &block)
-      matcher.(result, &block) and return if block_given? # evaluate user blocks first.
-      matcher.(result, &handlers)     # then, generic Rails handlers in controller context.
-    end
+    def call(options, overrides)
+      overrides.each do |rule_key, rule_description|
+        rule = rule_description[:rule] || DEFAULT_MATCHERS[rule_key][:rule]
+        resolve = rule_description[:resolve] || DEFAULT_MATCHERS[rule_key][:resolve]
+        if rule.nil? || resolve.nil?
+          puts "Matcher is not properly set. #{rule_key} will be ignored"
+          next
+        end
 
-    def matcher
-      Matcher
-    end
-
-    module Controller
-      # endpoint(Create) do |m|
-      #   m.not_found { |result| .. }
-      # end
-      def endpoint(operation_class, options={}, &block)
-        handlers = Handlers::Rails.new(self, options).()
-        Endpoint.(operation_class, handlers, *options[:args], &block)
+        return resolve.(options[:result], options[:representer]) if rule.(options[:result])
       end
+      matching_rules(overrides).each do |rule_key, rule_description|
+        if rule_description[:rule].(options[:result])
+          return rule_description[:resolve].(options[:result], options[:representer])
+        end
+      end
+    end
+
+    def matching_rules(overrides)
+      DEFAULT_MATCHERS.except(*overrides.keys)
     end
   end
 end
