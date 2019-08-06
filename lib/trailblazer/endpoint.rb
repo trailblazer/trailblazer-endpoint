dcdd71c98ebf46db3103b609841ccd82437657b0
Rui Baltazar <rui.p.baltazar@gmail.com>
2019-08-06 12:24:16 +0800

    WIP - added result extender

--- lib/trailblazer/endpoint.rb
+++ lib/trailblazer/endpoint.rb
@@ -91,4 +91,28 @@ module Trailblazer
       DEFAULT_MATCHERS.except(*overrides.keys)
     end
   end
+
+  class Operation
+    class Result
+      def errors
+        return self["result.policy.failure"] if policy_error?
+        return self["contract.default"].errors.full_messages if contract_error?
+        return self["result.model.errors"] if model_error?
+
+        self[:errors]
+      end
+
+      def policy_error?
+        self["result.policy.failure"].present?
+      end
+
+      def model_error?
+        self["result.model.errors"].present?
+      end
+
+      def contract_error?
+        self["contract.default"].present?
+      end
+    end
+  end
 end
