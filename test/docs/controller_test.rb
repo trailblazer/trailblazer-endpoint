5fcc799f04bb2db892385b1bfb399f81725ae4ac
Nick Sutterer <apotonick@gmail.com>
2020-07-29 11:06:56 +0200

    remove useless test.

--- test/docs/controller_test.rb
+++ test/docs/controller_test.rb
@@ -105,6 +105,7 @@ class DocsControllerTest < Minitest::Spec
       end
     end
 
+    # standard use case: {success} and {failure}
     def update
       _endpoint "view?" do |ctx, seq:, **|
         render "success" + ctx[:current_user] + seq.inspect
@@ -219,95 +220,8 @@ class ControllerOptionsTest < Minitest::Spec
     end
   end
 
-  it "what" do
-
-  end
-
   it "allows to get options without a bloody controller" do
     MemoController.bla(params: params)
   end
 end
 
-
-class DocsControllerTest < Minitest::Spec
-  it "what" do
-    endpoint "view?" do |ctx|
-      # 200, success
-      return
-    end
-
-    # 422
-    # but also 404 etc
-  end
-
-
-  class Controller
-    def initialize(endpoint, activity)
-      @___activity = activity
-      @endpoint = endpoint
-      @seq      = []
-    end
-
-    def view(params)
-      endpoint "view?", params do |ctx|
-        @seq << :success
-        # 200, success
-        return
-      end.Or() do |ctx|
-        # Only 422
-        @seq << :failure
-      end
-    end
-
-    def call(action, **params)
-      send(action, **params)
-      @seq
-    end
-
-    private def endpoint(action, params, &block)
-      ctx = Trailblazer::Endpoint.advance_from_controller(@endpoint,
-        event_name:             "",
-        success_block: block,
-        failure_block: ->(*) {
-          @seq << :failure_block
-          return },
-        protocol_failure_block: ->(*) { @seq << 401 and return },
-
-        domain_ctx: {},
-        flow_options: {},
-
-        **params,
-
-      # DISCUSS: do we really like that fuzzy API? if yes, why do we need {additional_endpoint_options} or whatever it's called?
-        seq: @seq,
-
-        or_status: [422, :failure]
-      )
-    end
-  end
-
-  it "injected {return} interrupts the controller action" do
-    protocol = Class.new(Trailblazer::Endpoint::Protocol)do
-      include T.def_steps(:authenticate, :policy)
-    end
-
-    endpoint =
-      Trailblazer::Endpoint.build(
-        domain_activity: activity,
-        protocol: protocol,
-        adapter: Trailblazer::Endpoint::Adapter::Web,
-        scope_domain_ctx: false,
-
-    ) do
-      {Output(:not_found) => Track(:not_found)}
-    end
-
-  # 200
-    seq = Controller.new(endpoint, activity).call(:view)
-    seq.must_equal [:authenticate, :policy, :model, :validate, :success] # the {return} works.
-
-  # 401
-    seq = Controller.new(endpoint, activity).call(:view, authenticate: false)
-    seq.must_equal [:authenticate, 401]
-  end
-end
