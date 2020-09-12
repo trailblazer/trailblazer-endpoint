846eeb6940f7b14b8aabf7f83257cbcf3ad440a5
Yogesh Khater <yogeshjain999@gmail.com>
2020-09-12 10:34:17 +0530

    Make test rails app NOT API only 🤦

--- test/rails-app/config/application.rb
+++ test/rails-app/config/application.rb
@@ -33,6 +33,6 @@ module RailsApp
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
   end
 end
