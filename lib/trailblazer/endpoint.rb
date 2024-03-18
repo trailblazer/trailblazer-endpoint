3ea7d185a8d62b69c3edef7a8ca71ed640353423
Nick Sutterer <apotonick@gmail.com>
2024-03-18 08:49:54 +0100

    remove outdated code.

--- lib/trailblazer/endpoint.rb
+++ lib/trailblazer/endpoint.rb
@@ -31,62 +31,8 @@ module Trailblazer
         )
       end
     end
-
-    def self.scope_domain_ctx
-      {
-        Activity::Railway.In()  => ->(ctx, **) { ctx[:domain_ctx] }, # gets automatically Context()'ed.
-        Activity::Railway.Out() => ->(domain_ctx, **) { {:domain_ctx => domain_ctx} }
-      }
-    end
-
-    #@ For WORKFLOW and operations. not sure this method will stay here.
-    def self.arguments_for(domain_ctx:, flow_options:, circuit_options: {}, **endpoint_options)
-      # we don't have to create the Ctx wrapping explicitly here. this is done via `:input`.
-      # domain_ctx      = Trailblazer::Context::IndifferentAccess.build(domain_ctx, {}, [domain_ctx, flow_options], circuit_options)
-
-      [
-        [
-          {
-              domain_ctx:                     domain_ctx, # DISCUSS: is this where {:resume_data} comes in?
-              # process_model_class:            process_model_class,
-              # process_model_from_resume_data: process_model_from_resume_data,
-              # find_process_model:             find_process_model,
-              # encrypted_resume_data:          encrypted_resume_data,
-
-              # cipher_key:                     cipher_key,
-              **endpoint_options,
-          },
-          flow_options
-        ],
-        circuit_options
-      ]
-    end
-
-    # FIXME: name will change! this is for controllers, only!
-    def self.advance_from_controller(endpoint, success_block:, failure_block:, protocol_failure_block:, **argument_options)
-      args = Trailblazer::Endpoint.arguments_for(argument_options)
-
-      signal, (ctx, _ ) = Trailblazer::Endpoint.with_or_etc(
-        endpoint,
-        args[0], # [ctx, flow_options]
-
-        success_block:          success_block,
-        failure_block:          failure_block,
-        protocol_failure_block: protocol_failure_block,
-      )
-
-      ctx
-    end
   end
 end
-#       created: Dry::Matcher::Case.new(
-#         match:   ->(result) { result.success? && result["model.action"] == :new }, # the "model.action" doesn't mean you need Model.
-#         resolve: ->(result) { result }),
-#       not_found: Dry::Matcher::Case.new(
-#         match:   ->(result) { result.failure? && result["result.model"] && result["result.model"].failure? },
-#         resolve: ->(result) { result }),
-#       # TODO: we could add unauthorized here.
-
 
 require "trailblazer/endpoint/protocol"
 require "trailblazer/endpoint/adapter"
