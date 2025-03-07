5db32e7c7e72447d3afcd97550cb27015b7e79da
Nick Sutterer <apotonick@gmail.com>
2025-03-07 09:31:46 +0100

    more controller unit tests.

--- test/controller_unit_test.rb
+++ test/controller_unit_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Controller_Test < Minitest::Spec
+class ControllerWithoutProtocolTest < Minitest::Spec
   class Controller
     attr_reader :input
 
@@ -48,18 +48,32 @@ class Controller_Test < Minitest::Spec
           ctx[:model] = Module
         end
       end
+
+      class Update < Trailblazer::Activity::Railway
+        step :model, Output(:failure) => End(:not_found)
+
+        include T.def_steps(:model)
+      end
     end
   end
 
-  it "invoke can call an operation without any endpoint logic involved" do
-    controller_class = Class.new(Controller) do
-      # DISCUSS: you *always* have to set a canonical invoke in a Rails app.
-      kernel = Class.new do
-        Trailblazer::Invoke.module!(self)
-      end.new
+  let(:kernel) { # DISCUSS: you *always* have to set a canonical invoke in a Rails app.
+    Class.new do
+      Trailblazer::Invoke.module!(self)
+    end.new
+  }
 
+  def controller(&block)
+    kernel = self.kernel
+
+    Class.new(Controller) do
       Trailblazer::Endpoint::Controller.module!(self, canonical_invoke: kernel.method(:__)) # DISCUSS: we always call via circuit interface (for matchers) so this is fine.
+      class_eval(&block)
+    end
+  end
 
+  it "invoke can call an activity without any endpoint logic involved, and no directive configured" do
+    controller_class = controller do
       def create
         invoke Memo::Operation::Create, seq: [], **input do
           success { |ctx, model:, **| render model.inspect }
@@ -68,14 +82,26 @@ class Controller_Test < Minitest::Spec
       end
     end
 
-    assert_runs(
-      controller_class,
-      :create,
+    assert_runs(controller_class, :create,
 
       success:            {render: %(Module)},
-      # not_authorized:     {render: %(not authorized: Object), policy: false},
-      # not_authenticated:  {render: %(authentication failed), authenticate: false},
       failure:            {render: %(failure), validate: false}
+    )
+  end
+
+  it "allows matchers for custom termini" do
+    controller_class = controller do
+      def update
+        invoke Memo::Operation::Update, seq: [], **input do
+          success { |ctx, seq:, **| render seq.inspect }
+          not_found { |ctx, seq:, **| render "404 #{seq.inspect}" }
+        end
+      end
+    end
+
+    assert_runs(controller_class, :update,
+      success:   {render: %([:model])},
+      not_found: {render: %(404 [:model]), model: false}
     )
   end
 end
