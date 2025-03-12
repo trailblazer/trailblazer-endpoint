bf3e9f49cdea807b028fd333d5944fb8f051e34f
Nick Sutterer <apotonick@gmail.com>
2025-03-12 12:02:19 +0100

    test more options.

--- test/endpoint_test.rb
+++ test/endpoint_test.rb
@@ -53,7 +53,7 @@ class EndpointTest < ControllerTest
     )
   end
 
-  it "allows identical endpoint constants with different {:name}" do
+  it "allows identical endpoint constants with different name using {:domain_activity}" do
     protocol = self.protocol
 
     controller_class = Class.new(application_controller) do
@@ -77,4 +77,118 @@ class EndpointTest < ControllerTest
     assert_runs(controller_class, :create_again, **expected_outcomes)
   end
 
+  describe "activity stopping on all four termini" do
+    class Create < Trailblazer::Activity::FastTrack
+      step :model, Output(:failure) => End(:not_found)
+      step :validate, fast_track: true
+
+      include T.def_steps(:model, :validate)
+    end
+
+    it "can be configured using {fast_track_to_railway}, {protocol_block}" do
+      protocol = Class.new(self.protocol) do
+        terminus :fail_fast
+        terminus :pass_fast
+      end
+
+      controller_class = Class.new(application_controller) do
+        endpoint "explicit fast_track", domain_activity: Create, protocol: protocol
+        endpoint "binary", domain_activity: Create, fast_track_to_railway: true, protocol: protocol # fast track outputs are wired to railway termini.
+        endpoint "custom wiring", domain_activity: Create, protocol: protocol do
+          {
+            Output(:fail_fast) => End(:failure),
+            Output(:pass_fast) => End(:success),
+            Output(:success) => End(:failure),
+            Output(:not_found) => End(:fail_fast),
+          }
+        end
+        # re-introduce the {fail_fast} outcome manually,
+        # after using {fast_track_to_railway},
+        # implying that we can mix and override wiring options.
+        endpoint "binary and custom wiring", domain_activity: Create, fast_track_to_railway: true, protocol: protocol do
+          {
+            Output(:not_found) => End(:fail_fast),
+          }
+        end
+
+        def create
+          invoke "explicit fast_track", protocol: true do
+            success   { |ctx, **| render "success" }
+            fail_fast { |*| render "yay, fast_track!" }
+            failure   { |*| render "failure" }
+            pass_fast { |*| render "hooray, pass_fast!" }
+            not_found { |*| render "404" }
+          end
+        end
+
+        def with_binary
+          invoke "binary", protocol: true do
+            success   { |ctx, **| render "success" }
+            failure   { |*| render "failure" }
+            not_found { |*| render "404" }
+          end
+        end
+
+        def with_custom_wiring
+          invoke "custom wiring", protocol: true do
+            success   { |ctx, **| render "success" }
+            failure   { |*| render "failure" }
+            fail_fast { |*| render "404" }
+          end
+        end
+
+        def with_binary_and_custom_wiring
+          invoke "binary and custom wiring", protocol: true do
+            success   { |ctx, **| render "success" }
+            failure   { |*| render "failure" }
+            fail_fast { |*| render "fail_fast" }
+          end
+        end
+      end
+
+    # explicit fast track
+      assert_runs(
+        controller_class, :create,
+
+        success:            {render: %(success)},
+        failure:            {render: %(failure), validate: false},
+        fail_fast:          {render: %(yay, fast_track!), validate: Trailblazer::Activity::FastTrack::FailFast},
+        pass_fast:          {render: %(hooray, pass_fast!), validate: Trailblazer::Activity::FastTrack::PassFast},
+        not_found:          {render: %(404), model: false},
+      )
+
+        # binary, fast_track gets routed to railway
+      assert_runs(
+        controller_class, :with_binary,
+
+        success:            {render: %(success)},
+        failure:            {render: %(failure), validate: false},
+        fail_fast:          {render: %(failure), validate: Trailblazer::Activity::FastTrack::FailFast},
+        pass_fast:          {render: %(success), validate: Trailblazer::Activity::FastTrack::PassFast},
+        not_found:          {render: %(404), model: false},
+      )
+
+    # custom wiring
+    assert_runs(
+        controller_class, :with_custom_wiring,
+
+        success:            {render: %(failure)},
+        failure:            {render: %(failure), validate: false},
+        fail_fast:          {render: %(failure), validate: Trailblazer::Activity::FastTrack::FailFast},
+        pass_fast:          {render: %(success), validate: Trailblazer::Activity::FastTrack::PassFast},
+        not_found:          {render: %(404), model: false},
+      )
+
+    # binary and custom wiring
+      assert_runs(
+        controller_class, :with_binary_and_custom_wiring,
+
+        success:            {render: %(success)},
+        failure:            {render: %(failure), validate: false},
+        fail_fast:          {render: %(failure), validate: Trailblazer::Activity::FastTrack::FailFast},
+        pass_fast:          {render: %(success), validate: Trailblazer::Activity::FastTrack::PassFast},
+        not_found:          {render: %(fail_fast), model: false},
+      )
+    end
+  end
 end
